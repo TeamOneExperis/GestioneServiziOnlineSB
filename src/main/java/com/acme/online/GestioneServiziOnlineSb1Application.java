f1352a86828bf7dfb24a3f9c5617314ffa79c856
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-28 15:30:55 +0200

    first commit

---
+++ src/main/java/com/acme/online/GestioneServiziOnlineSb1Application.java
@@ -0,0 +1,13 @@
+package com.acme.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneServiziOnlineSb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneServiziOnlineSb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/online/GestioneServiziOnlineSb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneServiziOnlineSb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
